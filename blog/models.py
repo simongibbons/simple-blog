0719580ae47daf278e5725147c59bc5f37c138b2
Simon Gibbons <simongibbons@gmail.com>
2016-06-02 16:39:28 +0100

    Added Blog Post data model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    """ Data model for a blog post """
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+
+    creation_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
