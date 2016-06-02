3582438584696800753ce45c926a59d643f6ecee
Simon Gibbons <simongibbons@gmail.com>
2016-06-02 17:24:55 +0100

    Added tests for Post model and checks to see if a post should be displayed

--- blog/models.py
+++ blog/models.py
@@ -12,5 +12,25 @@ class Post(models.Model):
     creation_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
+    def publish(self, publish_date=None):
+        """ Publish a post at a specific time, defaults to now """
+        if publish_date is None:
+            publish_date = timezone.now()
+        self.published_date = publish_date
+        self.save()
+
+    def unpublish(self):
+        """ Clears the published time removing the post from the public site """
+        self.published_date = None
+        self.save()
+
+    def is_published(self):
+        """ Is the post published? """
+        return self.published_date is not None
+
+    def is_displayed(self):
+        """ Is the post displayable? """
+        return self.is_published() and self.published_date <= timezone.now()
+
     def __str__(self):
         return self.title
--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,66 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+from .models import Post
+
+
+class TestPost(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user("test_user")
+
+    def create_post(self):
+        post = Post()
+        post.author = self.user
+        post.title = "Test Post"
+        post.save()
+        post.refresh_from_db()
+        return post
+
+    def test_post_defaults_unpublished(self):
+        post = self.create_post()
+        assert post.published_date is None
+
+    def test_publish_now(self):
+        post = self.create_post()
+        post.publish()
+
+        assert post.is_published()
+        assert post.is_displayed()
+
+    def test_publish_in_past(self):
+        post = self.create_post()
+
+        publish_date = timezone.now() - datetime.timedelta(days=1)
+        post.publish(publish_date)
+        post.refresh_from_db()
+
+        assert post.is_published()
+        assert post.is_displayed()
+        assert post.published_date == publish_date
+
+    def test_publish_in_future(self):
+        post = self.create_post()
+
+        publish_date = timezone.now() + datetime.timedelta(days=1)
+        post.publish(publish_date)
+
+        assert post.is_published()
+        assert not post.is_displayed()
+        assert post.published_date == publish_date
+
+    def test_unpublishing_a_post(self):
+        post = self.create_post()
+        post.publish()
+        post.refresh_from_db()
+
+        assert post.is_published()
+
+        post.unpublish()
+        post.refresh_from_db()
+
+        assert not post.is_published()
+        assert not post.is_displayed()
+
