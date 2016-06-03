79ccc95187126923b3cf92259e35d3206e44f968
Simon Gibbons <simongibbons@gmail.com>
2016-06-03 11:17:32 +0100

    Added UI for authenticating users

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+import django.contrib.auth.views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/login', django.contrib.auth.views.login, name='login'),
+    url(r'^accounts/logout', django.contrib.auth.views.logout, {'next_page': '/'}, name='logout'),
     url(r'', include('blog.urls')),
 ]
