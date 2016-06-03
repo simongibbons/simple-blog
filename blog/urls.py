6e8ac84d0de79eba1b92cff64661dd4886e7a6a9
Simon Gibbons <simongibbons@gmail.com>
2016-06-03 10:41:58 +0100

    Added post detail view

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list')
+    url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail')
 ]
