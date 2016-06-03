3f4ae43f77082e5d56c21a2330f0a80a893fa203
Simon Gibbons <simongibbons@gmail.com>
2016-06-03 11:47:48 +0100

    Add page for draft posts

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ app_name = 'blog'
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
-    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail')
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/drafts/$', views.post_drafts, name='post_drafts'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
 from .models import Post
 
@@ -14,3 +15,12 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+@login_required()
+def post_drafts(request):
+    posts = Post.objects.\
+        filter(published_date__isnull=True).\
+        order_by('creation_date')
+
+    return render(request, 'blog/post_list.html', {'posts' : posts})
